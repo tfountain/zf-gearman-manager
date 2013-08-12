b831a3fe4d3ade282c2abf3c5f1b0f8e0d40889d
Tim Fountain <tim@tfountain.co.uk>
2013-08-12 13:34:24 +0100

    Added README

--- bin/pecl_manager.php
+++ bin/pecl_manager.php
@@ -1,10 +1,17 @@
 #!/usr/bin/env php
 <?php
+// NOTE: this script is provided as an example only. You will want to copy
+// this to your application root so that the relative paths are correct
 
-if(!class_exists("GearmanManager\Bridge\GearmanPeclManager")){
-    require __DIR__."/../src/ZfGearmanManager/ZfGearmanPeclManager.php";
-}
+chdir(dirname(__DIR__));
+
+// Setup autoloading
+require 'init_autoloader.php';
+
+// initialise the application (but don't run it)
+$app = Zend\Mvc\Application::init(require 'config/application.config.php');
 
 declare(ticks = 1);
 
-$gm = new ZfGearmanManager\ZfGearmanPeclManager();
+$gm = $app->getServiceManager()->get('ZfGearmanPeclManager');
+$gm->start();
